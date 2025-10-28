a8bdfe86175bb525023991a7619d5cae692b9b56
unknown <tejas@gstempire.com>
2025-10-28 15:09:10 +0530

    new

--- final/settings.py
+++ final/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -128,11 +128,19 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Directory where collectstatic will collect all static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories Django looks for static files
 STATICFILES_DIRS = [
-    BASE_DIR , "static/",
+    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "admin_dashboard/static"),
     BASE_DIR , "admin_dashboard/static/",
 
 ]
+
+# WhiteNoise compression and caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
